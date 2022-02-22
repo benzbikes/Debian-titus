9bc03e39850dc7346b40ea065742d0485b98e08c
benzbikes <51802377+benzbikes@users.noreply.github.com>
2022-02-22 09:58:23 -0600

    Add brave install and start login manager at boot

--- root.sh
+++ root.sh
@@ -11,13 +11,17 @@ builddir=$(pwd)
 # Add Custom Titus Rofi Deb Package
 dpkg -i 'Custom Packages/rofi_1.7.0-1_amd64.deb'
 
-# fetch GPG key
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-# add Brave repository
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-# update repositories cache
-sudo apt update
-# install Brave from configured repository
+
+# Install Brave Repo
+    # enable repositories over https
+    sudo apt install apt-transport-https curl
+    # fetch GPG key
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    # add Brave repository
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    # update repositories cache
+    sudo apt update
+    # install Brave from configured repository
 
 # Update packages list
 apt update
@@ -43,4 +47,7 @@ cp .Xresources /home/$username
 cp .Xnord /home/$username
 cp -R dotfiles/* /home/$username/.config/
 chown -R $username:$username /home/$username
+
+# Enable login manager
+sudo systemctl enable sddm
 

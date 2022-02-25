83b5dd8b78f06836d92b10a8c4a73ef75691510f
benzbikes <51802377+benzbikes@users.noreply.github.com>
2022-02-25 11:00:35 -0600

    set variety firstrun

--- user.sh
+++ user.sh
@@ -3,14 +3,17 @@
 # Appearance pacakges
 
 #Ms-fonts
-sudo apt install ttf-mscorefonts-installer
+sudo apt install ttf-mscorefonts-installer -y
 # Add feather font
 #cp fonts/Feather.ttf ~/.fonts/
 #fc-cache -vf
 
+touch ~/.config/variety/.firstrun
 # Layan Cursors
 mkdir -p $HOME/build
 cd "$HOME/build"
 git clone https://github.com/vinceliuice/Layan-cursors
 cd Layan-cursors
 sudo ./install.sh
+
+sudo systemctl enable sddm --now
